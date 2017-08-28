9a5e597b21aff86b8b64f541065a0887a8e8725e
user <user@Kevin>
2017-08-27 22:17:27 -0500

    Creada y modificada Calculador

---
+++ calculador/src/calc/Calculador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+/**
+ *
+ * @author user
+ */
+public class Calculador {
+    float operador1;
+    float operador2;
+    float resultado;
+    
+    void sumar(){
+        resultado=operador1+operador2;
+    }
+    
+    void restar(){
+        resultado=operador1-operador2;
+    }
+}
